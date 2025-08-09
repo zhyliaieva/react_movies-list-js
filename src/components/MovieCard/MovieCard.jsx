775d615b31c41070134a5e1fe103c2ce0ca9f8f4
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-08-09 19:16:54 +0300

    fix key and logo url

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -12,7 +12,7 @@ export const MovieCard = ({ movie }) => (
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src={movie.imdbUrl} alt="imdb" />
+            <imp src="../../public/images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
 
